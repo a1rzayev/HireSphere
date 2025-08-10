fc4eb15e15f4d9df4c878d8458824367b5f6abda
a1rzayev <asifrzayev2006@gmail.com>
2025-08-10 17:14:31 +0400

    Configure UserRepository working with EfCore

---
+++ api/HireSphere.Infrastructure/Repositories/UserEfCoreRepository.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HireSphere.Core.Models;
+using HireSphere.Infrastructure.ORM;
+
+namespace HireSphere.Infrastructure.Repositories;
+
+public class UserEfCoreRepository
+{
+    private readonly HireSphereDbContext _context;
+    private readonly DbSet<User> _dbSet;
+
+    public UserEfCoreRepository(HireSphereDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<User>();
+    }
+
+    public async Task<User?> GetByIdAsync(Guid id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<User>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _dbSet.AddAsync(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(User user)
+    {
+        _dbSet.Update(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var user = await GetByIdAsync(id);
+        if (user != null)
+        {
+            _dbSet.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _dbSet.FirstOrDefaultAsync(u => u.Email == email);
+    }
+}
