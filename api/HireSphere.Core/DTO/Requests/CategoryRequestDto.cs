6b3fa615f630ab90b2de9a53bcb8a0b26e09c3dd
a1rzayev <asifrzayev2006@gmail.com>
2025-08-10 16:20:59 +0400

    Configure Category(Request-Response)DTO's

---
+++ api/HireSphere.Core/DTO/Requests/CategoryRequestDto.cs
@@ -0,0 +1,6 @@
+namespace HireSphere.Core.DTO.Requests;
+
+public class CategoryRequestDto
+{
+    public string Name { get; set; } = string.Empty;
+}
---
+++ api/HireSphere.Core/DTO/Responses/CategoryResponseDto.cs
@@ -0,0 +1,8 @@
+namespace HireSphere.Core.DTO.Responses;
+
+public class CategoryResponseDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
+}
