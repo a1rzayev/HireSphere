ea75a687b8285db043f2790e47873abf38439dad
a1rzayev <asifrzayev2006@gmail.com>
2025-08-10 19:25:07 +0400

    Configure table for RefreshTokens in DbContext

--- api/HireSphere.Infrastructure/Migrations/20250810152423_AddRefreshTokenEntity.Designer.cs
+++ api/HireSphere.Infrastructure/Migrations/20250810152423_AddRefreshTokenEntity.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using HireSphere.Infrastructure.ORM;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HireSphere.Infrastructure.Migrations
 {
     [DbContext(typeof(HireSphereDbContext))]
-    partial class HireSphereDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250810152423_AddRefreshTokenEntity")]
+    partial class AddRefreshTokenEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- api/HireSphere.Infrastructure/ORM/HireSphereDbContext.cs
+++ api/HireSphere.Infrastructure/ORM/HireSphereDbContext.cs
@@ -12,9 +12,27 @@ public class HireSphereDbContext : DbContext
     public DbSet<Job> Jobs { get; set; }
     public DbSet<JobApplication> JobApplications { get; set; }
     public DbSet<Category> Categories { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<RefreshToken>(entity =>
+        {
+            entity.HasKey(rt => rt.Id);
+            entity.Property(rt => rt.Token).IsRequired().HasMaxLength(500);
+            entity.Property(rt => rt.ExpiryDate).IsRequired();
+            entity.Property(rt => rt.IsRevoked).IsRequired();
+            entity.Property(rt => rt.CreatedAt).IsRequired();
+            entity.Property(rt => rt.UserId).IsRequired();
+
+            entity.HasOne(rt => rt.User)
+                  .WithMany()
+                  .HasForeignKey(rt => rt.UserId)
+                  .OnDelete(DeleteBehavior.Cascade);
+
+            entity.HasIndex(rt => rt.Token).IsUnique();
+        });
     }
 }
