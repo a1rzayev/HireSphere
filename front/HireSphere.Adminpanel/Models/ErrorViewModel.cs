5e9fdc1f8120defc30aab2cde7ed12c1fdb80ea5
a1rzayev <asifrzayev2006@gmail.com>
2025-08-14 17:09:49 +0400

    Create Adminpanel layer

---
+++ front/HireSphere.Adminpanel/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HireSphere.Adminpanel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
